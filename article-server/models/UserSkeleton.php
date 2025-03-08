f515fea7d5c5da0d625493ef420da3da1c958453
Hussein-02 <hussein02ab@gmail.com>
2025-03-08 10:26:41 +0200

    finished getters and setters of user skeleton

---
+++ article-server/models/UserSkeleton.php
@@ -0,0 +1,57 @@
+<?php
+
+class UserSkeleton
+{
+
+    private $id;
+    private $fullname;
+    private $email;
+    private $password;
+
+    public function __construct($id = null, $fullname = null, $email = null, $password = null)
+    {
+        $this->id = $id;
+        $this->fullname = $fullname;
+        $this->email = $email;
+        $this->password = $password;
+    }
+
+    //getters/setters
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getFullName()
+    {
+        return $this->fullname;
+    }
+
+    public function setFullName($fullname)
+    {
+        $this->fullname = $fullname;
+    }
+
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
+
+    //crud methods to be implemented in model
+
+}
