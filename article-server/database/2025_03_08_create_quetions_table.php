f4aa9f2598f3ddb98cf88baa26d11ebd083227fb
Hussein-02 <hussein02ab@gmail.com>
2025-03-08 09:33:17 +0200

    finished creating the migrations

---
+++ article-server/database/2025_03_08_create_quetions_table.php
@@ -0,0 +1,22 @@
+<?php
+
+require_once '../connection/connection.php';
+
+function run_migration($conn)
+{
+    $sql = "
+    CREATE TABLE IF NOT EXISTS questions (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        question VARCHAR(255) NOT NULL,
+        answer VARCHAR(255) NOT NULL
+    );
+    ";
+
+    if ($conn->query($sql) == TRUE) {
+        echo "Questions table created successfullu!";
+    } else {
+        echo "Error creating table";
+    }
+}
+
+run_migration($conn);
