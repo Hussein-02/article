f9a6126b7c9c087e175123181c6b786a0c1608bb
Hussein-02 <hussein02ab@gmail.com>
2025-03-09 14:37:50 +0200

    finished question page and tested

---
+++ article-client/js/question.js
@@ -0,0 +1,33 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.querySelector("#questionForm");
+
+  form.addEventListener("submit", async function (e) {
+    e.preventDefault();
+
+    const question = document.getElementById("question").value;
+    const answer = document.getElementById("answer").value;
+    try {
+      const response = await axios.post(
+        "http://15.188.75.137/api/v1/addQuestion.php",
+        {
+          question: question,
+          answer: answer,
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      if (response.data.success) {
+        window.location.href = "/home.html";
+      } else {
+        alert(response.data.message);
+      }
+    } catch (error) {
+      console.error("There wan an error!", error);
+      alert("Login failed. Please try again.");
+    }
+  });
+});
